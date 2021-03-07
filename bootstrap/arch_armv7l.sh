8bb924951b86b9468f477468dc6f059612d87227
Joan R <jrbellido@gmail.com>
2021-03-07 11:39:22 +0100

    add bootstrap scripts

---
+++ bootstrap/arch_armv7l.sh
@@ -0,0 +1,5 @@
+broot
+vim
+neovim
+zsh
+
---
+++ bootstrap/arch_x86_64.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+sudo pacman -Sy \
+  base-devel -R automake
+
+sudo pacman -Sy \
+  alacritty \
+  bash \
+  bc \
+  broot  \
+  bzip2 \
+  cifs-utils \
+  darktable \
+  ffmpeg \
+  firefox \
+  htop \
+  i3-gaps \
+  i3blocks \
+  i3lock \
+  i3status \
+  inetutils \
+  inkscape \
+  iputils \
+  irssi \
+  jdk11-openjdk \
+  jq \
+  neovim \
+  restic \
+  ripgrep \
+  rofi \
+  rsync \
+  thunar \
+  thunar-archive-plugin \
+  thunar-volman \
+  tmux \
+  tree \
+  wget \
+  xfce4-appfinder \
+  xfce4-clipman-plugin \
+  xfce4-panel \
+  xfce4-power-manager \
+  xfce4-pulseaudio-plugin \
+  xfce4-screenshooter \
+  xfce4-session \
+  xfce4-settings \
+  xfce4-terminal \
+  zola \
+  zsh \
+
+
+# yay packages: yay, joplin, google-chrome

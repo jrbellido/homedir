c0b14c9908ad42128544a938a064bbcc8056f51d
Joan R <jrbellido@gmail.com>
2021-03-08 22:44:00 +0100

    add macOS bootstrap script

---
+++ bootstrap/macos_darwin.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+brew install \
+  zsh \
+  wget \
+  tmux \
+  htop \
+  gcc \
+  gdb \
+  automake \
+  autoconf \
+  cmake \
+  broot \
+  ffmpeg \
+  coreutils \
+
+brew install \
+  neovim \
+
+brew install \
+  rustup-init \
+
+brew install \
+  zola \
+
